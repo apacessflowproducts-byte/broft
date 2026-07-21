478bb5b0b135311299cf319395d2632c9ab1c4ca
apacessflowproducts-byte <ap.acessflowproducts@gmail.com>
2026-07-21 14:51:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const allowedOrigins = [
   'https://boisterous-queijadas-409afa.netlify.app',
   'https://oyzer.netlify.app',
   'https://mratz.netlify.app',
-  'https://sunny-pastelito-8843b7.netlify.app',
+  'https://fraqt.netlify.app',
   'https://viralbell.netlify.app',
   'https://jhvnb.vercel.app',
    // add all your trusted frontends here
