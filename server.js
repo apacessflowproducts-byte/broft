772fe24c92c5de0e2a603d19bcecd06dbf2c6b76
apacessflowproducts-byte <ap.acessflowproducts@gmail.com>
2026-08-11 19:11:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 const allowedOrigins = [
   'http://127.0.0.1:5000',
   'https://tremz.netlify.app',
-  'https://oyzer.netlify.app',
+  'https://freqz.netlify.app',
   'https://mratz.netlify.app',
   'https://yamz.netlify.app',
   'https://gredq.netlify.app',
